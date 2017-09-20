5ef892c613cee684b83476c2ebf4a481173a15b7
cunhapassos <cunha.passos@gmail.com>
2017-09-20 13:00:56 -0300

    Alteração na estrutura do projeto

--- main.c
+++ main.c
@@ -17,5 +17,5 @@ int main(int argc, const char * argv[]) {
     printf("Hello, World!\n");
     
     EX_imprimirClassFile(LE_lerArquivo("/Users/paulopassos/Documents/GitHub/JVM-SB-2-2017/LEITOR-EXIBIDOR/File.class"));
-    //return 0;
+    return 0;
 }
